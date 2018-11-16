633c4d420eb856be5fd28e974224122fbaef641b
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-16 12:01:32 +0100

    add registry.sol

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -1,10 +1,41 @@
 pragma solidity 0.4.25;
 
+import 'github.com/openzeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol';
+
 /**
-@title Ocean Protocol ERC20 Token Contract
+@title Fitchain Actor Registry
 @author Team: Fitchain Team
 */
 
-contract Registry {
+contract Registry is Ownable {
+
+    struct Registrant {
+        bool exists;
+        bool isAvailable;
+        uint8 actorType;
+    }
+
+    mapping(address => Registrant) registrants;
+
+    modifier canDeregister() {
+        require(!registrants[msg.sender].isAvailable, 'Registrant is busy, can not dergister');
+        _;
+    }
+
+    modifier isNotRegistered() {
+        require(!registrants[msg.sender].exists, 'Registrant already exists');
+        _;
+    }
+
+    function registerActor(uint8 actorType) public isNotRegistered() returns(bool) {
+        registrants[msg.sender] = Registrant(true, false, actorType);
+        return true;
+    }
+
+
+    function deregisterActor() public canDeregister() returns (bool) {
+        registrants[msg.sender].exists = false;
+        return true;
+    }
 
 }
