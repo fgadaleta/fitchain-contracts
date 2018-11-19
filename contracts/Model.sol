e1d4243a39a0bd95372e51f3e1293b542d2188aa
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-19 10:54:13 +0100

    add release stake in model.sol

--- contracts/Model.sol
+++ contracts/Model.sol
@@ -8,7 +8,7 @@ import './Stake.sol';
 @author Team: Fitchain Team
 */
 
-contract FitchainModel is FitchainRegistry, FitchainStake {
+contract FitchainModel is FitchainStake {
 
     // fitchain model
     struct Model {
@@ -26,4 +26,43 @@ contract FitchainModel is FitchainRegistry, FitchainStake {
     }
 
     mapping(bytes32 => Model) models;
+    uint256 private minStake;
+
+    //events
+    event ModelCreated(bytes32 modelId, address owner, bool state);
+
+    modifier notExist(bytes32 modelId){
+        require(!models[modelId].exist, 'Model already exist');
+        _;
+    }
+
+    modifier onlyValidatedModel(bytes32 modelId){
+        require(models[modelId].location != bytes32(0), 'model not exists');
+        require(models[modelId].isTrained, 'Model not trained yet!');
+        // require(models[modelId].isVerfied, 'Model not verified yet!');
+        _;
+    }
+
+    constructor(uint256 _minStake) public {
+        minStake = _minStake;
+    }
+
+    function createModel(bytes32 modelId, bytes32 paymentRecieptId) public notExist(modelId) returns(bool) {
+        if(super.stake(modelId, msg.sender, minStake)){
+            models[modelId] = Model(true, false, false, 0,
+                                    msg.sender, bytes32(0),
+                                    paymentRecieptId, bytes32(0),
+                                    bytes32(0), new bytes(0),
+                                    new string(0));
+            emit ModelCreated(modelId, msg.sender, true);
+            return true;
+        }
+        emit ModelCreated(modelId, msg.sender, false);
+        return false;
+
+    }
+
+    function releaseStake(bytes32 modelId) public onlyValidatedModel(modelId) returns(bool) {
+        return super.release(modelId, models[modelId].owner, minStake);
+    }
 }
