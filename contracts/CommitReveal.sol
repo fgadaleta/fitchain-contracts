db387fbc6961bebaef3af993e98bab12e2ffa882
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-20 16:01:21 +0100

    finalize commit-reveal contract

--- contracts/CommitReveal.sol
+++ contracts/CommitReveal.sol
@@ -8,6 +8,7 @@ pragma solidity 0.4.25;
 contract CommitReveal {
 
     struct Commitment{
+        bool exist;
         bool vote;
         bytes32 hash;
         string value;
@@ -22,6 +23,7 @@ contract CommitReveal {
     mapping(bytes32 => Setting) settings;
 
     event CommitmentInitialized(bytes32 commitmentId, uint256 timeout, address[] voters);
+    event CommitmentCommitted(bytes32 commitmentId, address voter);
 
     function setup(bytes32 _commitmentId, uint256 _timeout, address[] _voters) internal returns(bool){
         require(_timeout > 0, 'Indicating invalid timeout');
@@ -30,8 +32,19 @@ contract CommitReveal {
         return true;
     }
 
-    function commit(bytes32 commitmentId, bytes32 hash) public returns(bool);
+    function commit(bytes32 _commitmentId, bytes32 _hash) public returns(bool){
+        require(!commitments[_commitmentId][msg.sender].exist, 'avoid replay attack');
+        commitments[_commitmentId][msg.sender] = Commitment(true, false, _hash, new string(0));
+        emit CommitmentCommitted(_commitmentId, msg.sender);
+        return true;
+    }
 
-    function reveal(bytes32 commitmentId, string value, bool vote) public returns(bool);
+    function reveal(bytes32 _commitmentId, string _value, bool _vote) public returns(bool){
+        require(!commitments[_commitmentId][msg.sender].exist);
+        require(commitments[_commitmentId][msg.sender].hash == keccak256(abi.encodePacked(_vote, _value)));
+        commitments[_commitmentId][msg.sender].vote = _vote;
+        commitments[_commitmentId][msg.sender].value = _value;
+        return true;
+    }
 
 }
