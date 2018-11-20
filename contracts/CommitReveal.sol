b3ce4af06cc96699179d716f493b3f7d91ef1495
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-20 11:54:14 +0100

    init commit reveal scheme

---
+++ contracts/CommitReveal.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.4.25;
+
+/**
+@title Fitchain Commit Reveal Scheme
+@author Team: Fitchain Team
+*/
+
+contract CommitReveal {
+
+}
--- contracts/VerifiersPool.sol
+++ contracts/VerifiersPool.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.4.25;
 
 import './FitchainRegistry.sol';
+import './CommitReveal.sol';
 
 /**
 @title Fitchain Model Verifiers Pool
 @author Team: Fitchain Team
 */
 
-contract VerifiersPool is FitchainRegistry {
+contract VerifiersPool is FitchainRegistry, CommitReveal {
 
 }
