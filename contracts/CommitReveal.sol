74a3575de1017e2c2a19006f08838e4da7f7adbc
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-20 15:50:41 +0100

    implement setup phase in commit-reveal

--- contracts/CommitReveal.sol
+++ contracts/CommitReveal.sol
@@ -7,4 +7,31 @@ pragma solidity 0.4.25;
 
 contract CommitReveal {
 
+    struct Commitment{
+        bool vote;
+        bytes32 hash;
+        string value;
+    }
+
+    struct Setting{
+        uint256 timeout;
+        address owner;
+    }
+
+    mapping(bytes32 => mapping(address => Commitment)) commitments;
+    mapping(bytes32 => Setting) settings;
+
+    event CommitmentInitialized(bytes32 commitmentId, uint256 timeout, address[] voters);
+
+    function setup(bytes32 _commitmentId, uint256 _timeout, address[] _voters) internal returns(bool){
+        require(_timeout > 0, 'Indicating invalid timeout');
+        settings[_commitmentId] = Setting(_timeout, msg.sender);
+        emit CommitmentInitialized(_commitmentId, _timeout, _voters);
+        return true;
+    }
+
+    function commit(bytes32 commitmentId, bytes32 hash) public returns(bool);
+
+    function reveal(bytes32 commitmentId, string value, bool vote) public returns(bool);
+
 }
