3464ce0d35141921f2d5ff78a93d407897bf82eb
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-21 16:30:31 +0100

    add end of processing and trigger commitment phase

--- contracts/VerifiersPool.sol
+++ contracts/VerifiersPool.sol
@@ -14,6 +14,7 @@ contract VerifiersPool is FitchainHelper, CommitReveal, FitchainRegistry  {
     struct Challenge{
         bool exist;
         uint256 wallTime;
+        uint256 canCommit;
         bytes32 modelId;
         bytes32 proofId;
         bytes32 testingData;
@@ -29,16 +30,31 @@ contract VerifiersPool is FitchainHelper, CommitReveal, FitchainRegistry  {
     struct VPCSetting{
         uint256 minKVerifiers;
         uint256 minStake;
+        uint256 commitTimeout;
+        uint256 revealTimeout;
     }
 
     mapping (address => VPCSetting) VPCsettings;
     mapping (bytes32 => Challenge) challenges;
     mapping(bytes32 => Proof) proofs;
+    mapping(bytes32 => mapping (address => uint256)) voteOnce;
+
 
     // events
-    event ChallengeInitialized(bytes32 challengeId, address[] verifiers, bytes32 proofId);
+    event ChallengeInitialized(bytes32 challengeId, address[] verifiers, bytes32 proofId, bytes32 testingData);
+    event CommitPhaseStarted(bytes32 challengeId, address[] verifiers);
 
     // modifiers
+    modifier onlyChallengeVerifiers(bytes32 challengeId){
+        bool isVerifier = false;
+        require(challenges[challengeId].exist, 'Challenge does not exist!');
+        for(uint256 i=0; i < challenges[challengeId].verifiers.length; i++){
+            if(msg.sender == challenges[challengeId].verifiers[i]) isVerifier = true;
+        }
+        require(isVerifier, 'sender is not a verifier for this challenge');
+        _;
+    }
+
     modifier onlyValidStake(uint256 amount){
         require(amount >= VPCsettings[address(this)].minStake);
         _;
@@ -59,9 +75,14 @@ contract VerifiersPool is FitchainHelper, CommitReveal, FitchainRegistry  {
         _;
     }
 
+    modifier onlyCanVoteOnce(bytes32 challengeId) {
+        require(voteOnce[challengeId][msg.sender] < 1, 'indicating replay attack for voting');
+        _;
+    }
+
     // init VPC settings
-    constructor(uint256 _minKVerifiers, uint256 _minStake) public {
-        VPCsettings[address(this)] = VPCSetting(_minKVerifiers, _minStake);
+    constructor(uint256 _minKVerifiers, uint256 _minStake, uint256 _commitTimeout, uint256 _revealTimeout) public {
+        VPCsettings[address(this)] = VPCSetting(_minKVerifiers, _minStake, _commitTimeout, _revealTimeout);
     }
 
     function initChallenge(bytes32 modelId, bytes32 challengeId, uint256 wallTime,
@@ -69,16 +90,26 @@ contract VerifiersPool is FitchainHelper, CommitReveal, FitchainRegistry  {
         require(wallTime > 20, 'invalid wallTime, should be at least greater than average block interval');
         address[] memory verifiers = getAvailableVerifiers();
         if(verifiers.length >= kVerifiers){
-            challenges[challengeId] = Challenge(true, wallTime, modelId, challengeId, testingData, msg.sender, new address[](0));
+            challenges[challengeId] = Challenge(true, block.timestamp + wallTime, 0,modelId, challengeId, testingData, msg.sender, new address[](0));
             require(kVerifiers == getKVerifiers(challengeId, kVerifiers), 'unable to set verifiers for challenge');
             for (uint256 i=0; i < challenges[challengeId].verifiers.length; i++){
                 decrementActorSlots(challenges[challengeId].verifiers[i]);
             }
-            emit ChallengeInitialized(challengeId, challenges[challengeId].verifiers, challengeId);
+            // now verifiers can start validation using
+            emit ChallengeInitialized(challengeId, challenges[challengeId].verifiers, challengeId, testingData);
             return true;
         }
         return false;
 
+    }
+
+    function endOfProcessingPhase(bytes32 challengeId) public onlyChallengeVerifiers(challengeId) onlyCanVoteOnce(challengeId) {
+        voteOnce[challengeId][msg.sender] +=1;
+        challenges[challengeId].canCommit +=1;
+        if (challenges[challengeId].canCommit == challenges[challengeId].verifiers.length){
+            emit CommitPhaseStarted(challengeId, challenges[challengeId].verifiers);
+            super.setup(challengeId, VPCsettings[address(this)].commitTimeout, VPCsettings[address(this)].revealTimeout, challenges[challengeId].verifiers);
+        }
     }
 
     function getAvailableVerifiers() private view returns(address[]){
--- migrations/6_deploy_verifiers_pool.js
+++ migrations/6_deploy_verifiers_pool.js
@@ -1,6 +1,6 @@
 /* global artifacts */
 const VerifiersPool = artifacts.require('VerifiersPool.sol')
 const verifiersPool = async (deployer, network) => {
-    await deployer.deploy(VerifiersPool, 3, 1)
+    await deployer.deploy(VerifiersPool, 3, 1, 40, 40)
 }
 module.exports = verifiersPool
