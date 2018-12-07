9b51bc8561004fa823cbc2e4aaa1e069ba1540e5
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-12-07 11:54:06 +0100

    fix registry contract

--- contracts/FitchainRegistry.sol
+++ contracts/FitchainRegistry.sol
@@ -14,30 +14,29 @@ contract FitchainRegistry is Ownable {
         bool exists;
         uint256 slots;
         uint256 maxSlots;
-        address registryOwner;
     }
-
-    mapping(address => Registrant) registrants;
-    address[] actors;
+    // RegistryOwner -> Actor -> RegistrantStruct
+    mapping(address => mapping(address => Registrant)) registry;
+    mapping(address => address[]) actors;
     FitchainStake private staking;
 
-    modifier onlyRegistryOwner(address registrant){
-        require(registrants[registrant].registryOwner == msg.sender, 'invalid registry owner');
+    modifier onlyRegistryOwner(address actor){
+        require(registry[msg.sender][actor].exists, 'invalid registry owner');
         _;
     }
 
     modifier onlyExist(address actor){
-        require(registrants[actor].exists, 'Actor does not exists!');
+        require(registry[msg.sender][actor].exists, 'invalid registry owner');
         _;
     }
 
     modifier onlyFreeSlots(address actor){
-        require(registrants[actor].slots == registrants[actor].maxSlots, 'registrant is busy, please free slots!');
+        require(registry[msg.sender][actor].slots == registry[msg.sender][actor].maxSlots, 'registrant is busy, please free some slots');
         _;
     }
 
     modifier onlyNotExist(address actor) {
-        require(!registrants[actor].exists, 'Actor already exists!');
+        require(!registry[msg.sender][actor].exists, 'Actor already exists!');
         _;
     }
 
@@ -49,16 +48,16 @@ contract FitchainRegistry is Ownable {
     function register(address actor, uint256 slots, bytes32 stakeId, uint256 amount) public onlyNotExist(actor) returns(bool) {
         require(slots >= 1, 'invalid number of free slots');
         require(staking.stake(stakeId, actor, slots * amount));
-        registrants[actor] = Registrant(true, slots, slots, msg.sender);
-        addActorToRegistry(actor);
+        registry[msg.sender][actor] = Registrant(true, slots, slots);
+        addActorToRegistry(actor, msg.sender);
         return true;
     }
 
     function deregister(address actor, bytes32 stakeId) public onlyRegistryOwner(actor) onlyFreeSlots(actor) returns (bool) {
         uint256 amount = staking.getStakebyActor(stakeId, actor);
         require(amount > 0, 'indicating empty stake!');
-        registrants[actor].exists = false;
-        removeActorFromRegistry(actor);
+        registry[msg.sender][actor].exists = false;
+        removeActorFromRegistry(actor, msg.sender);
         return staking.release(stakeId, actor, amount);
     }
 
@@ -67,60 +66,61 @@ contract FitchainRegistry is Ownable {
         if(decrementSlots) {
             decrementActorSlots(actor);
             // decrement the max slots after slashing the stake
-            registrants[actor].maxSlots -=1;
+            registry[msg.sender][actor].maxSlots -=1;
         }
         return true;
     }
 
     function isActorRegistered(address actor) public view returns(bool) {
-        return registrants[actor].exists;
+        return registry[msg.sender][actor].exists;
     }
 
     function getActorFreeSlots(address actor) public view returns(uint256) {
-        return registrants[actor].slots;
+        return registry[msg.sender][actor].slots;
     }
 
     function getActorMaxSlots(address actor) public view returns(uint256) {
-        return registrants[actor].maxSlots;
+        return registry[msg.sender][actor].maxSlots;
     }
 
-    function removeActorFromRegistry(address actor)  private returns(bool) {
-        for(uint256 j=0; j<actors.length; j++){
-            if(actor == actors[j]){
-                for (uint i=j; i< actors.length-1; i++){
-                    actors[i] = actors[i+1];
+    function removeActorFromRegistry(address actor, address owner)  private returns(bool) {
+        // This function is prone to gas limit errors
+        for(uint256 j=0; j<actors[owner].length; j++){
+            if(actor == actors[owner][j]){
+                for (uint i=j; i< actors[owner].length-1; i++){
+                    actors[owner][i] = actors[owner][i+1];
                 }
-                actors.length--;
+                actors[owner].length--;
                 return true;
             }
         }
         return false;
     }
 
-    function addActorToRegistry(address actor) private returns(bool) {
-        actors.push(actor);
+    function addActorToRegistry(address actor, address owner) private returns(bool) {
+        actors[owner].push(actor);
         return true;
     }
 
     function getAvaliableRegistrants() public view returns(address[]) {
-        return actors;
+        return actors[msg.sender];
     }
 
     function decrementActorSlots(address actor) public onlyRegistryOwner(actor) returns(bool){
-        require(registrants[actor].slots > 0, 'invalid slots value');
-        registrants[actor].slots -=1;
-        if(registrants[actor].slots == 0){
-            return removeActorFromRegistry(actor);
+        require(registry[msg.sender][actor].slots > 0, 'invalid slots value');
+        registry[msg.sender][actor].slots -=1;
+        if(registry[msg.sender][actor].slots == 0){
+            return removeActorFromRegistry(actor, msg.sender);
         }
         return true;
     }
 
     function incrementActorSlots(address actor) public onlyRegistryOwner(actor) returns(bool){
-        require(registrants[actor].slots >=0, 'invalid slots value');
-        if(registrants[actor].slots == 0){
-            addActorToRegistry(actor);
+        require(registry[msg.sender][actor].slots >=0, 'invalid slots value');
+        if(registry[msg.sender][actor].slots == 0){
+            addActorToRegistry(actor, msg.sender);
         }
-        registrants[actor].slots +=1;
+        registry[msg.sender][actor].slots +=1;
         return true;
     }
 }
